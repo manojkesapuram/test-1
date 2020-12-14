5762f818ad7c78642b4716dbaf15803a7aadb095
manoj <manojkesapuram@gmail.com>
2020-12-14 17:01:38 +0530

    commit test1

---
+++ src/test/java/com/marsApp/TestMavenGit1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMavenGit1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
